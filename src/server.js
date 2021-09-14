bd42a52b486e2041cfb81ae7db573caf04bb44bc
HajnalAron <79903244+HajnalAron@users.noreply.github.com>
2021-09-14 17:06:09 +0200

    Fixed path

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
 import express from "express";
 import listEndpoints from "express-list-endpoints";
 import cors from "cors";
-import authorsRouter from "./services/authors/authors/";
+import authorsRouter from "./services/authors/index.js";
 
 const server = express();
 const port = 3001;
 
 server.use(cors());
 server.use(express.json());
-server.use("./services/authors/index.js", authorsRouter);
+server.use("/authors", authorsRouter);
 
 console.table(listEndpoints(server));
 
